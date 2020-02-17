e7d2bca041a4af9793ec245e5438b89ef1d4ba01
Evangelos Giataganas <e.giataganas@gmail.com>
2020-02-17 01:16:24 +0000

    Update todo completed on click

--- src/App.js
+++ src/App.js
@@ -10,10 +10,26 @@ class App extends Component {
     this.state = {
       todos: todosData
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: todosData
+      }
+    })
+
   }
 
   render() {
-    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
 
     return (
       <div className="todo-list">
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,11 @@ function TodoItem(props) {
   console.log(props)
   return (
     <div className="todo-item">
-      <input type="checkbox"  checked={props.item.completed} />
+      <input
+          type="checkbox"
+          checked={props.item.completed}
+          onChange={() => props.handleChange(props.item.id)}
+      />
       <p>{props.item.text}</p>
     </div>
   )
